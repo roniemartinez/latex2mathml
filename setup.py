b04d631d7a78cafc76c22b9fb90b6ac03d54e39f
Ronie Martinez <ronmarti18@gmail.com>
2021-06-02 23:24:03 +0200

    🔖 Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='latex2mathml',
-    version='3.6.0-alpha.1',
+    version='3.6.0',
     description='Pure Python library for LaTeX to MathML conversion',
     python_requires='<4,>=3.6.2',
     project_urls={"repository": "https://github.com/roniemartinez/latex2mathml"},
