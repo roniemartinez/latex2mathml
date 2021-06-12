e2b975c78dc067d13fd30136ab9b96a1a40d9a6a
Ronie Martinez <ronmarti18@gmail.com>
2021-06-12 21:30:11 +0200

    🐛 Fix \centerdot

--- latex2mathml/symbols_parser.py
+++ latex2mathml/symbols_parser.py
@@ -18,23 +18,23 @@ def parse_symbols() -> Dict[str, str]:
     _symbols: Dict[str, str] = {}
     with codecs.open(SYMBOLS_FILE, encoding="utf-8") as f:
         for line in f:
-            if not line.startswith("#"):
-                columns = line.strip().split("^")
-                _unicode = columns[0]
-                latex = columns[2]
-                unicode_math = columns[3]
-                if latex and latex not in _symbols:
-                    _symbols[latex] = _unicode
-                if unicode_math and unicode_math not in _symbols:
-                    _symbols[unicode_math] = _unicode
-                for equivalent in re.findall(r"=\s+(\\[^,^ ]+),?", columns[-1]):
-                    if equivalent not in _symbols:
-                        _symbols[equivalent] = _unicode
+            if line.startswith("#"):
+                continue
+            columns = line.strip().split("^")
+            _unicode = columns[0]
+            latex = columns[2]
+            unicode_math = columns[3]
+            if latex and latex not in _symbols:
+                _symbols[latex] = _unicode
+            if unicode_math and unicode_math not in _symbols:
+                _symbols[unicode_math] = _unicode
+            for equivalent in re.findall(r"[=#]\s*(\\[^,^ ]+),?", columns[-1]):
+                if equivalent not in _symbols:
+                    _symbols[equivalent] = _unicode
     _symbols.update(
         {
             r"\bigcirc": _symbols[r"\lgwhtcircle"],
             r"\Box": _symbols[r"\square"],
-            r"\centerdot": _symbols[r"\cdot"],
             r"\circledS": "024C8",
             r"\diagdown": "02572",
             r"\diagup": "02571",
